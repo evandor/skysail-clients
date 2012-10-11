f51399ecad7f12744cdd506220c4287d80a455bc
Carsten Gräf <evandor@gmail.com>
2012-10-11 23:08:26 +0200

    wip

--- skysail.client.dbviewer.wicket/src/main/java/de/twenty11/skysail/client/dbviewer/wicket/DbViewerHome.java
+++ skysail.client.dbviewer.wicket/src/main/java/de/twenty11/skysail/client/dbviewer/wicket/DbViewerHome.java
@@ -3,9 +3,12 @@ package de.twenty11.skysail.client.dbviewer.wicket;
 import java.util.List;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 
+import de.twenty11.skysail.client.dbviewer.wicket.pages.ConnectionsPage;
+import de.twenty11.skysail.client.dbviewer.wicket.templates.DbViewerTemplate;
 import de.twenty11.skysail.common.ext.dbviewer.ConnectionDetails;
 
 public class DbViewerHome extends DbViewerTemplate {
@@ -18,14 +21,13 @@ public class DbViewerHome extends DbViewerTemplate {
     public DbViewerHome() {
         add(new Label("oneComponent", "Welcome7"));
         add(new ListView("connections", getConnections()) {
-
             @Override
             protected void populateItem(ListItem item) {
                 ConnectionDetails connection = (ConnectionDetails) item.getModelObject();
                 item.add(new Label("connectionName", connection.getId()));
             }
-
         });
+        add(new BookmarkablePageLink("addConnection", ConnectionsPage.class));
     }
 
     private List<ConnectionDetails> getConnections() {
--- skysail.client.dbviewer.wicket/src/main/java/de/twenty11/skysail/client/dbviewer/wicket/templates/DbViewerTemplate.java
+++ skysail.client.dbviewer.wicket/src/main/java/de/twenty11/skysail/client/dbviewer/wicket/templates/DbViewerTemplate.java
@@ -1,4 +1,4 @@
-package de.twenty11.skysail.client.dbviewer.wicket;
+package de.twenty11.skysail.client.dbviewer.wicket.templates;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
